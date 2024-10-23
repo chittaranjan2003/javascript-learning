16cdb9b94376f8846f0036de97ef3bb6c30a490a
chittaranjan2003 <141479077+chittaranjan2003@users.noreply.github.com>
2024-10-23 11:17:37 +0530

    variable-assignment push
    variable-assignment push

---
+++ variable-assignment.js
@@ -0,0 +1,8 @@
+var cityName="Delhi";
+console.log("City Name :",cityName);
+var bestFriend="munu";
+console.log("best friend name :",bestFriend);
+var schoolName="Talakusuma high school";
+console.log( "School Name is", schoolName);
+var college="Niis";
+console.log("Collage Name :",college);
