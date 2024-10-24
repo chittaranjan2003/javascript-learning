6ec3cdfdd01960d58fc180869a47a2d47f30d33e
chittaranjan2003 <141479077+chittaranjan2003@users.noreply.github.com>
2024-10-24 23:02:17 +0530

    assignments02 push
    assignments02 push

---
+++ assignments02.js
@@ -0,0 +1,20 @@
+function myfun() {
+  console.log("Hello JS !");
+}
+myfun();
+
+function personalDetails(firstName, lastName, collegeName) {
+  console.log("My first Name :", "chitta");
+  console.log("Last Name :", "swain");
+  console.log("My collage Name :", " Niis");
+}
+var Details = personalDetails();
+function addThreeValues(n1,n2,n3)
+{
+    var res=n1+ n2+n3;
+    console.log("Addition of three Number is :",res);
+
+}
+addThreeValues(10,20,10);
+addThreeValues("Hello",  "Good","Morning");
+
