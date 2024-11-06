e79bda69d834486102a2a8d46d868996fd6f0729
chittaranjan2003 <141479077+chittaranjan2003@users.noreply.github.com>
2024-11-06 18:41:12 +0530

    07_switchCaseMonth push
    07_switchCaseMonth push

---
+++ loop_ass_06.js
@@ -0,0 +1,30 @@
+/*for (var index = 5; index <= 15; index++) {
+  console.log(index);
+}
+
+for (var index = 50; index >= 40; index--) {
+  console.log(index);
+}*/
+/*for (var index = 1; index <= 30; index=index+2) {
+    console.log(index);
+}*/
+
+/*for (var index = 2; index <=20; index=index+2) {
+    console.log(index);
+        
+}*/
+
+/*for (var index = 5; index <=50; index=index+5) {
+     
+    console.log(index);
+}*/
+
+/*for (var index = 10; index <= 100; index=index+10) {
+    
+    console.log(index);
+}*/
+
+for (var index = 100; index>= 10; index=index-10) {
+    
+    console.log(index);
+}
