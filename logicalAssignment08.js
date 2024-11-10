1cde705662016d5f0699a7bb7c0741562dc29759
chittaranjan2003 <141479077+chittaranjan2003@users.noreply.github.com>
2024-11-10 21:24:24 +0530

    logicalAssignment08 push
    logicalAssignment08 push

--- logicalAssignment08.js
+++ logicalAssignment08.js
@@ -1,24 +1,26 @@
 let array = [10, 20, 45, 30, 77, 60];
+let count1 = 0;
 for (const element of array) {
-  if (element % 2 == 1) {
-    console.log(`Given array Even Number is :${element}`);
+  if (element % 2 == 0) {
+    count1 = count1 + 1;
   }
 }
+console.log(`Given array Even Number is :${count1}`);
 
 const myLove = "I Love only javaScript";
 const vowels = "aeiou";
-let count =0;
+let count2 = 0;
 for (const char of myLove) {
   if (vowels.includes(char.toLowerCase())) {
-    count = count + 1;
+    count2 = count2 + 1;
   }
 }
-console.log(`Vowels count is: ${count}`);
-const sentence="JAVASCRIPT LANGUAGE";
-let count2=0;
+console.log(`Vowels count is: ${count2}`);
+
+const sentence = "JAVASCRIPT LANGUAGE";
+let count3 = 0;
 for (const char of sentence) {
-    if(char.toLocaleLowerCase()=="a")
-    count2=count2+1;
-    
+  if (char.toLocaleLowerCase() == "a") 
+    count3 = count3 + 1;
 }
-console.log(`Total character a is ${count2}`);
+console.log(`Total character a is ${count3}`);
